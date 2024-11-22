af132d153811a8a97bde4c0d22567ca10c118fdc
Somi Yoo <79749558+blue-ssom@users.noreply.github.com>
2024-11-22 19:43:29 +0900

    fix: 서버에서 index.html 파일을 반환하도록 수정

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require("express")
 const dotenv = require('dotenv').config();
-
 const app = express() 
 const port = process.env.PORT
 
@@ -8,7 +7,7 @@ app.use(express.json())
 
 // 테스트 라우트
 app.get('/', (req, res) => {
-    res.send('Hello, World!');
+    res.sendFile(__dirname + '/index.html'); // __dirname을 사용해 현재 경로를 기반으로 파일을 제공
 });
 
 app.listen(port, () => {
